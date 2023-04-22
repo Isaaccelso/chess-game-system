764ac0c890980272a111ab006535b99dbbb1ef7b
Isaac Celso <isaac.celsoa@gmail.com>
2023-04-22 15:35:42 -0300

    Chess layer and print board

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,12 @@
 package application;
 
+import Chess.ChessMatch;
+import boardgame.Board;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
     }
 }
